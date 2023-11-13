93b52c75a2d9037f0b870a42803573d3a96999ea
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-11-14 00:37:26 +0100

    Making a server with http

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-    
+const http = require('http');
+
+//Create Server objcet
+
+http.createServer((request, response) => {
+    //write a response
+    response.write('hello wrld');
+    response.end();
+}).listen(8080, () => console.log('Server running'));
