24c543bd2ee8570192762abdcdbef0e13b7d22b9
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-11-14 11:01:00 +0100

    404 page done

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ const server = http.createServer((req, res) => {
             res.writeHead(200,  { 'Content-Type': 'text/html'});
             res.end(content)
         })
-    }
-
-    if(req.url === '/about'){
+    } else if(req.url === '/about'){
         fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
             if(err) throw err;
             res.writeHead(200,  { 'Content-Type': 'text/html'});
             res.end(content)
         })
-    }
-
-    if(req.url === '/contact-me'){
+    } else if(req.url === '/contact-me'){
         fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), (err, content) => {
+            if(err) throw err;
+            res.writeHead(200,  { 'Content-Type': 'text/html'});
+            res.end(content)
+        })
+    } else {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
             if(err) throw err;
             res.writeHead(200,  { 'Content-Type': 'text/html'});
             res.end(content)
